911a8fab9d0bdb2d96faa0ec4e04d8acc3dbdd40
Maryna Kolesnykova <marynakolesnykovam@gmail.com>
2026-02-20 15:32:41 +0100

    frontend fix

--- app/main.py
+++ app/main.py
@@ -39,8 +39,7 @@ def create_app() -> FastAPI:
 
     # Register routers here
     app.include_router(sumo_router)
-    app.include_router(sumo_proxy_router)
-
+    app.include_router(sumo_proxy_router, prefix="/sumo")
     return app
 
 
